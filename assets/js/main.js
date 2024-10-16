8a82c307521bd09b5d26c83bc85e65d3d51ec749
Archar1os <148558039+Archar1os@users.noreply.github.com>
2024-10-17 00:27:25 +0800

    Added view ports, hamburger js, navigation htmls fix and highlight on products in index

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,36 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
+	navToggle = document.getElementById("nav-toggle"),
+	navClose = document.getElementById("nav-close");
 
 if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
-  });
+	navToggle.addEventListener("click", () => {
+		navMenu.classList.add("show-menu");
+	});
+	navToggle.addEventListener("click", () => {
+		navToggle.classList.add("show-none");
+	});
+	navToggle.addEventListener("click", () => {
+		navClose.classList.remove("show-none");
+	});
 }
 if (navClose) {
-  navClose.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu");
-  });
+	navClose.addEventListener("click", () => {
+		navMenu.classList.remove("show-menu");
+	});
+	navClose.addEventListener("click", () => {
+		navToggle.classList.remove("show-none");
+	});
+	navClose.addEventListener("click", () => {
+		navClose.classList.add("show-none");
+	});
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
-  const navMenu = document.getElementById("nav-menu");
-  navMenu.classList.remove("show-menu");
+	const navMenu = document.getElementById("nav-menu");
+	navMenu.classList.remove("show-menu");
 };
 navLink.forEach((n) => n.addEventListener("click", linkAction));
